4369d6e1ca73b4be3930cc79c03d733bcd4e26a0
SaifullahAkhtar-001 <118631470+SaifullahAkhtar-001@users.noreply.github.com>
2022-12-05 02:17:43 +0500

    small counter app

---
+++ index.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("counter")
+let count = 0
+function increament(){
+    console.log(count)
+    count = count+1
+    countEl.innerText = count
+}
+
+function decreament(){
+    console.log(count)
+    count = count-1
+    countEl.innerText = count
+}
+
+function reset(){
+    console.log(count)
+    count = 0
+    countEl.innerText = count
+}
